9499863fbb6f1876001fb362d73ede6c436da208
isidurlll <isidurlll@gmail.com>
2015-06-05 18:23:18 +0200

    Vendedor Canvas

---
+++ Assets/Scripts/Utilidades/CanvasVendedor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasVendedor : MonoBehaviour {
+
+    public Transform anclaCanvas;
+    public Vector3 camera;
+    //public Vector3 posAnclaCanvas;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 posCamera = Camera.main.WorldToScreenPoint(anclaCanvas.position);
+        camera = posCamera;
+        transform.position = posCamera;
+	}
+}
