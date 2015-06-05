c657f28f61135151ec5dc4c7bea2492a4e36004f
JaviTalavera <javi.tm90@gmail.com>
2015-06-05 17:08:37 +0200

    - Detalles al mapa

---
+++ Assets/followPlayer.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class followPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+		if (Vector3.Distance(Utils.player.transform.position, transform.position) < 10) {
+			Vector3 lookAt = new Vector3(Utils.player.transform.position.x,
+			                             transform.position.y,
+			                             Utils.player.transform.position.z);
+			transform.LookAt(lookAt);
+		}
+	}
+}
